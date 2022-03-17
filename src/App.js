d02cd02809a93ba95f6e3ca23d41f67621bf875b
Muhammad Bilal Rizvi <bilal742742@gmail.com>
2022-03-18 01:04:56 +0500

    add documentation link and command to install redux
    add documentation link and command to install redux library

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import './App.css';
 import Counter from './components/Counter';
 import CounterDisplay from './components/CounterDisplay';
 
+// Using Redux with React Hooks ( Documentation Link )
+// https://thoughtbot.com/blog/using-redux-with-react-hooks
+// Commands to Install
+//	npx create-react-app my-app
+//	cd my-app
+//	yarn add redux react-redux
+//	npm start / yarn start
+
+
 function App() {
   return (
     <div >
